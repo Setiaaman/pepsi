724c9f2df41f08ce55c3130974d0c788ca45547b
Setiaaman <122644119+Setiaaman@users.noreply.github.com>
2023-01-14 01:56:22 +0530

    Add files via upload

---
+++ Project 3 - Pepsi landing page/index.js
@@ -0,0 +1,9 @@
+function imgslider(anything){
+    document.querySelector('.pepsi').src = anything;
+}
+
+function colorchange(color)
+{
+    const bgcolor = document.querySelector('.sec');
+    bgcolor.style.background=color;
+}
